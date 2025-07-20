c1bb4d70cb7062e310d524ee4db5b6016643279d
Hayley Bishop <hayleyabishop@gmail.com>
2025-07-20 16:52:18 -0500

    Reformatted .js file into a function that accepts chord list. Returns set of chord arrays. Added "let" in front of variable declarations for interval/chord arrays. Removed extra space.

--- webapp.js
+++ webapp.js
@@ -1,50 +1,33 @@
-
 //Event listeners
 
-// document.getElementById("chordInput1").addEventListener("change", handle)
+function calculateResultingChords(inputChords) {
+  const chords = ["A","A# / Bb", "B", "C", "C# / Db", "D", "D# / Eb", "E", "F", "F# / Gb", "G", "G# / Ab"];
+  const autoharpChords = ["A", "A# / Bb", "B", "C", "D", "D# / Eb", "E", "F", "G", "G# / Ab"];
+let autoharpIntervals = [];
 
-
-
-
-
-inputChords = ["C", "F# / Gb", "G"];
-console.log("Input Chords: \n" + inputChords);
-
-
-
-const chords = ["A","A# / Bb", "B", "C", "C# / Db", "D", "D# / Eb", "E", "F", "F# / Gb", "G", "G# / Ab"];
-const autoharpChords = ["A", "A# / Bb", "B", "C", "D", "D# / Eb", "E", "F", "G", "G# / Ab"];
-autoharpIntervals = [];
-
-intervals = [];
+let intervals = [];
 //Find the input chords' relationship to one another. 
 //Transpose that relationship by finding other chords in the autoharpChords list 
 //that have the same relationship. 
+console.log("Input Chords: \n" + inputChords);
 
-
-for (let i in autoharpChords){
- 
+  for (let i in autoharpChords){
     autoharpIntervals.push(chords.indexOf(autoharpChords[i]));
-
-}
+  }
 console.log("\n Available Autoharp Chords Index: \n" + autoharpIntervals)
 
 //Available Autoharp Chords Index: 0,1,2,3,5,6,7,8,10,11
 
-for (let i in inputChords){
-    //
+  for (let i in inputChords){
     intervals.push(chords.indexOf(inputChords[i]));
-    // console.log(intervals)
-}
+    console.log(intervals)
+  }
 
 //Subtract smallest chord number from all elements, to find lowest common denominator of intervals.
-intervals = intervals.map(element => element - intervals[0]); 
-
-matches = [];
+  intervals = intervals.map(element => element - intervals[0]); 
+  let matches = [];
 
 console.log("\nInput Chord Intervals: \n" + intervals);
-
-console.log()
 
 for(i=0;Math.max(...intervals)<12;i++){ //Try looking for all 3 items in the autoharp set; try again, going up by a half note, until the largest interval is 12.
 
@@ -53,28 +36,26 @@ for(i=0;Math.max(...intervals)<12;i++){ //Try looking for all 3 items in the aut
 
     //If all intervals are present, save this set as a match.
     if (foundIntervals.length == intervals.length){
-        matches.push(foundIntervals);
+      matches.push(foundIntervals);
     }
     intervals=intervals.map(element => element+1);
-}
-console.log("\nFound the following chord intervals: \n")
+  }
+
+  // Convert the index numbers in matches to the actual chord names.
+  let chordMatches = matches.map(match => match.map(index=>chords[index]))
 
 //Print each chord grouping, no matter how many matches there are.
 for (const match of matches) {
     console.log(match);
 }
-
+return chordMatches;
+}
 // foundIntervals = intervals.filter((interval) => autoharpIntervals.includes(interval));
-
 // console.log(intervals.filter((interval) => autoharpIntervals.includes(interval)))
-
+const resultingChords = calculateResultingChords(inputChords);
 
 console.log("\n\nI found the following matches: \n")
-
-//Convert the index numbers in matches to the actual chord names.
-chordMatches = matches.map(match => match.map(index=>chords[index]))
-
-for (const match of chordMatches) {
+for (const match of resultingChords) {
     console.log(match);
 }
 
